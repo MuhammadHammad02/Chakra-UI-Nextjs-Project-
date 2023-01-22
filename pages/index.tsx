879b3d59b2f67a84b279d8285d307622e3ac0afd
Muhammad Hammad <108385539+MuhammadHammad02@users.noreply.github.com>
2023-01-23 00:45:02 +0500

    Chakra NextJs Project

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Features  from './components/Feature';
+import Header  from './components/Header';
+import  Pricing  from './components/Price';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Pricing />
+      <Features />
+    </div>
+  );
+}
+
+export default App;
